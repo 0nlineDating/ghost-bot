2f56a5442fd3e39b874035962bdb0c85ddc10805
desiworm <desiworm1024@gmail.com>
2020-09-10 19:32:03 -0400

    bugfix
    Updating to fix lost SQLite db due to pm2. Replaced relative path with absolute path to solve the problem.

--- bot.js
+++ bot.js
@@ -12,8 +12,10 @@ const sequelize = new Sequelize('database', 'user', 'passsword', {
     host: 'localhost',
     dialect: 'sqlite',
     logging: true,
-    //SQLite only
-    storage: 'database.sqlite'
+    // SQLite only
+    // This is for an RPI, use an absolute path.
+    // Otherwise it won't find the database if you use pm2.
+    storage: '/home/pi/DiscordBot/database.sqlite'
     }
 );
 
@@ -42,7 +44,6 @@ const Tags = sequelize.define('tags', {
 /*
 // This only needs to be on when it's first initialized.
 // I ran into issues with the sync clearing the database on hard resets
-
 client.once('ready', () => {
  Tags.sync();
 })
@@ -86,8 +87,8 @@ client.on('message', async (receivedMessage) => {
         const input = receivedMessage.content.slice(PREFIX.length).trim().split(' ');
         const command = input.shift();
         const commandArgs = input.join(' ');
-	    // These are the basic commands that come with tag management.
         if (command === 'addtag') {
+            // [delta]
             const splitArgs = commandArgs.split(' ');
             const tagName = splitArgs.shift().toLowerCase();
             const tagDescription = splitArgs.join(' ');
@@ -108,6 +109,7 @@ client.on('message', async (receivedMessage) => {
                 return message.reply('Something went wrong with adding a tag.');
             }
         } else if (command === 'tag') {
+            // [epsilon]
             const tagName = commandArgs;
 
             // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
@@ -120,6 +122,7 @@ client.on('message', async (receivedMessage) => {
             return message.reply(`Could not find tag: ${tagName}`);            
         }
         else if (command === 'edittag') {
+            // [zeta]
             const splitArgs = commandArgs.split(' ');
             const tagName = splitArgs.shift();
             const tagDescription = splitArgs.join(' ');
@@ -131,6 +134,7 @@ client.on('message', async (receivedMessage) => {
             }
             return message.reply(`Could not find a tag with name ${tagName}.`);
         } else if (command === 'taginfo') {
+            // [theta]
             const tagName = commandArgs;
 
             // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
@@ -140,6 +144,7 @@ client.on('message', async (receivedMessage) => {
             }
             return message.reply(`Could not find tag: ${tagName}`);
         } else if (command === 'showtags') {
+            // [lambda]
             // equivalent to: SELECT name FROM tags;
             const tagList = await Tags.findAll({ attributes: ['name'] });
             const tagString = tagList.map(t => t.name).join(', ') || 'No tags set.';
@@ -158,32 +163,32 @@ client.on('message', async (receivedMessage) => {
             console.log('Lol no command given.');
         }
     }
-
-	// this is just because i thought it was funny, i'll be honest.
+        // this is just because i thought it was funny, i'll be honest.
 	// the basic tags can't provide images, but custom commands can.
 	// if you want to add in more image responses, they can be written in just by copying this block.
     if (messageLower.includes("dickbutt")) {
-	// Provide a URL to a file
+    // Provide a URL to a file
+    // I should just store this locally.
     const webAttachment = new Discord.MessageAttachment('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstyfelife.com%2Fwp-content%2Fuploads%2F2016%2F10%2Fdickbutt.png&f=1&nofb=1', "dickbuttlol.png");
     receivedMessage.channel.send(webAttachment);
     console.log(`Sent a dickbutt to ` + receivedMessage.channel.name.toString());
     }
-	//this is the part that parses every single message.
+    // this is the part that parses every single message.
     if (receivedMessage) {
-	    // converts text to lowercase and removes punctuation and markup
-	    // this prevents keywords from being missed in the event that they are proceeded by a period or question mark
+	// converts text to lowercase and removes punctuation and markup
+	// this prevents keywords from being missed in the event that they are proceeded by a period or question mark
         const input = receivedMessage.content.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-	    // this splits the message content into an array, where each word is its' own item in the array
+	// this splits the message content into an array, where each word is its' own item in the array
         var commandArgs = input.split(' ');
         console.log('INPUT IS: ' + commandArgs + ' | Length is: ' + commandArgs.length);
-        var i=0;
-	    // this gathers the length of the array and sets it to a variable (simplifies comparison)
+        let i=0;
+	// this gathers the length of the array and sets it to a variable (simplifies comparison)
         var end = commandArgs.length;
-	    //begins a loop through the array
-        while (i < end) {
+        // begins a loop through the array
+        do {
             console.log('i = ' + i);
             const tagName = commandArgs[i];
-            
+
             // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
             const tag = await Tags.findOne({ where: { name: tagName } });
             if (tag) {
@@ -195,13 +200,13 @@ client.on('message', async (receivedMessage) => {
                 console.log(`Could not find tag: ${tagName}`);
             }
             i++;
-        }
+        } while (i < end);
     }
     // This doesn't work yet. Eventually, it'll post an image when a secret word is said
     // the plan is to have it shuffle a set of secret words every week
     // working with canvas is hard though, so right now it just throws an error for every instance of the word test.
     // it's not an error that crashes the script, luckily.
-    if (receivedMessage.content.includes(SecretWord)) {
+    /*if (receivedMessage.content.includes(SecretWord)) {
         const channel = receivedMessage.channel;
 
         const canvas = Canvas.createCanvas(750,588);
@@ -219,14 +224,14 @@ client.on('message', async (receivedMessage) => {
         const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'secret-word.png');
 
         channel.send(`YOU SAID THE SECRET WORD, ${member}!`, attachment);
-    }
+    }*/
 })
 
 
 
 //Get your bot's secret token from:
 // https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to reveal token"
+// Click on your application -> Bot -> Token -> "Click hto reveal token"
 bot_secret_token = " ";
 
 client.login(bot_secret_token);
